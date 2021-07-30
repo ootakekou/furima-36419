246ac18aab48bbf3bcf8c82d910fd8aab58bc01a
ootakekou <kamanattou@gmail.com>
2021-07-30 19:49:55 +0900

    ユーザーバリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_kana, presence: true
-  validates :last_kana, presence: true
+  validates :first_kana, presence: true, format: { with: /\A[ァ-ンー－]+\z/ }
+  validates :last_kana, presence: true, format: {with: /\A[ァ-ンー－]+\z/ }
   validates :birthday, presence: true
 end
