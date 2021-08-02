c6f034d31bc6a1890b4b95963a1d7287412aaffd
ootakekou <kamanattou@gmail.com>
2021-08-02 16:43:09 +0900

    ner_item_pathへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except: :index
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2021_07_30_062027) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.integer "category_id", null: false
+    t.integer "money", null: false
+    t.text "text", null: false
+    t.integer "status_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_07_30_062027) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
