30cb0db3820737d9e438291b9b904b25228a1016
ootakekou <kamanattou@gmail.com>
2021-08-03 12:26:35 +0900

    バリテーション機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,11 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :product_name
-    validates :money
     validates :text
+    validates :image
+    with_options format: {with: /\A[0-9]+\z/} do
+      validates :money, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+    end
    with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
