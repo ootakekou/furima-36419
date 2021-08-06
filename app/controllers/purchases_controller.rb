2abb7f38dd8b6fa6aa584a629e9d8a908efef5ed
ootakekou <kamanattou@gmail.com>
2021-08-06 20:32:55 +0900

    クレジット機能の作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
 
   def index
+    
     @item = Item.find(params[:item_id])
     @purchase_delivery = PurchaseDelivery.new
   end
@@ -9,6 +10,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_delivery = PurchaseDelivery.new(purchase_params)
     if @purchase_delivery.valid?
+     pay_item
       @purchase_delivery.save
       redirect_to root_path
     else
@@ -18,6 +20,15 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_delivery).permit(:postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id).merge( user_id: current_user.id,item_id: @item.id)
+    params.require(:purchase_delivery).permit(:postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id).merge( user_id: current_user.id,item_id: @item.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.money,  
+        card: purchase_params[:token],    
+        currency: 'jpy'                 
+      )
   end
 end
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,6 +1,6 @@
 class PurchaseDelivery
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,6 +8,7 @@ class PurchaseDelivery
     validates :city
     validates :city_number
     validates :telephone, format: {with: /\A\d{10,11}\z/}
+    validates :token
   end
     def save
       purchase = Purchase.create(item_id: item_id, user_id: user_id)
