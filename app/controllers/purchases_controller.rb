b7a50822139da7f939db4feb7d9b3af446737a47
ootakekou <kamanattou@gmail.com>
2021-08-06 15:40:25 +0900

    購入情報保存機能の作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,23 @@
 class PurchasesController < ApplicationController
-  
+
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_delivery = PurchaseDelivery.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_delivery).permit(:postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id).merge( user_id: current_user.id,item_id: @item.id)
+  end
 end
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,3 +1,19 @@
 class PurchaseDelivery
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :city_number, :build_name, :telephone, :purchase_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :city_number
+    validates :telephone, format: {with: /\A\d{10,11}\z/}
+  end
+    def save
+      purchase = Purchase.create(item_id: item_id, user_id: user_id)
+  
+      Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, city_number: city_number, build_name: build_name, telephone: telephone, purchase_id: purchase.id)
+
+    end
+  
 end
