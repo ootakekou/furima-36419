52b5dfbdb34b58979ca53ca7e7a33c8b3603fc1a
ootakekou <kamanattou@gmail.com>
2021-08-08 20:39:56 +0900

    購入後のedit遷移制限作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show] 
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :sold_out_item, only: :edit
   def index
     @items = Item.order("created_at DESC").includes(:user)
   end
@@ -54,4 +55,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def sold_out_item
+    @item = Item.find(params[:id])
+   if @item.purchase.present?
+    redirect_to root_path
+   end
+   end
+   
 end
